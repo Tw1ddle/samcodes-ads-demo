860ce3bf7b77a33d05dde86102f111d5f498471b
sam <sam@silver42.karoo.co.uk>
2015-02-04 22:23:04 +0000

    Switch to placeholder admob ad locations

--- source/ads/AdLocations.hx
+++ source/ads/AdLocations.hx
@@ -8,9 +8,9 @@ class AdLocations {
 	public static inline var SAMPLE_MORE_APPS_LOCATION:String = "cb_more_apps";
 	public static inline var SAMPLE_REWARDED_VIDEO_LOCATION:String = "cb_rewarded_video";
 	#elseif admobads
-	public static inline var DEMO_BANNER_LOCATION:String = "ca-app-pub-2600848144826429/9122223592";
+	public static inline var DEMO_BANNER_LOCATION:String = "YOUR_ADMOB_BANNER_ID";
 	
-	public static inline var SAMPLE_INTERSTITIAL_LOCATION:String = "ca-app-pub-2600848144826429/1598956796";
+	public static inline var SAMPLE_INTERSTITIAL_LOCATION:String = "YOUR_ADMOB_INTERSTITIAL_ID";
 	public static inline var SAMPLE_MORE_APPS_LOCATION:String = "n/a";
 	public static inline var SAMPLE_REWARDED_VIDEO_LOCATION:String = "n/a";
 	#else
